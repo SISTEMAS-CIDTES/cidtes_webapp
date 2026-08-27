e7c0ef69f8a267775491bec3f91d0393ef182dde
marko161302-cmyk <marko161302@gmail.com>
2026-08-27 00:28:55 -0600

    Update page.tsx

--- app/capacitacion/page.tsx
+++ app/capacitacion/page.tsx
@@ -70,6 +70,16 @@ export default function CapacitacionPage() {
                   </p>
 
                   <p>Hemos capacitado a más de 40,000 personas, potencializando sus competencias laborales y las respaldamos a través de:</p>
+                  {/* Imagen de respaldo */}
+<div className="mt-6 flex justify-center">
+  <Image
+    src="/images/capacitacion/respaldamos.png"
+    alt="Respaldamos nuestras capacitaciones"
+    width={660}
+    height={916}
+    className="w-full max-w-[500px] h-auto object-contain"
+  />
+</div>
                 </div>
               </div>
           
@@ -319,3 +329,4 @@ export default function CapacitacionPage() {
     </div>
   )
 }
+
